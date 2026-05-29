c29d3fcc46b7d6754259eb2dc6d047bddb562531
Alejandro Bustos <sam.alejandro.bustos@gmail.com>
2026-05-29 00:41:01 -0300

    feat(input): implemented InputHandler

---
+++ Assets/Scripts/Input/InputHandler.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Input
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public static InputHandler Instance { get; private set; }
+        
+        // ? Variables
+        private Vector2 _movementInput;
+
+        // ? Events
+        public Action<Vector2> OnMove;
+        public Action OnJump;
+
+        // ? Components
+        private InputAction _inputAction;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
+            _inputAction = new InputAction();
+        }
+
+        public void OnMoveInput(InputAction.CallbackContext context)
+        {
+            _movementInput = context.ReadValue<Vector2>();
+            OnMove?.Invoke(_movementInput);
+        }
+
+        public void OnJumpInput(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                OnJump?.Invoke();
+            }
+        }
+    }
+}
