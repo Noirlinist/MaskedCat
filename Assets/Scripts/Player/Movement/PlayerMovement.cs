c255a55bacbf2ababd08ba3bf274c592ae0f570d
Alejandro Bustos <sam.alejandro.bustos@gmail.com>
2026-05-29 20:25:32 -0300

    feat(player): implemented character idle animation

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -14,16 +14,21 @@ namespace Player.Movement
         // Components
         private InputHandler _inputHandler;
         private Rigidbody2D _rigidbody2D;
+        private Animator _animator;
         
         private void Start()
         {
             _inputHandler = InputHandler.Instance;
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
 
         private void Update()
         {
             _movementInput = _inputHandler.MovementInput;
+
+            HandleRotation();
+            HandleAnimation();
             
             if(_movementInput != Vector2.zero)
                 Move(_movementInput.x);
@@ -32,6 +37,27 @@ namespace Player.Movement
         private void Move(float input)
         {
             _rigidbody2D.linearVelocityX = input * movementSpeed;
+        }
+
+        private void HandleAnimation()
+        {
+            if (_rigidbody2D.linearVelocity.x > 0.01 || _rigidbody2D.linearVelocity.x < -0.01)
+            {
+                
+            }
+        }
+        
+        private void HandleRotation()
+        {
+            if (_rigidbody2D.linearVelocityX < 0)
+            {
+                transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+            }
+
+            if (_rigidbody2D.linearVelocityX > 0)
+            {
+                transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+            }
         }
     }
 }
